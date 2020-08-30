4995c54dba419f8a339f086438561a9483d530a0
hushaorui <hushaorei@163.com>
2020-08-30 22:51:20 +0800

    确立客户端协议处理结构

--- src/main/resources/static/bootstrap/js/common.js
+++ src/main/resources/static/bootstrap/js/common.js
@@ -4,17 +4,114 @@ function timestampToDate(timestamp) {
     if ((typeof timestamp)  == "string") {
         timestamp = parseInt(timestamp.trim());
     }
-    var date = new Date(timestamp);
+    let date = new Date(timestamp);
     return dateToString(date);
 }
 // 将时间对象转换为可读字符串
 function dateToString(date) {
-    var y = date.getFullYear();
-    var m = date.getMonth()+1;
-    var d = date.getDate();
-    var h = date.getHours();
-    var mm = date.getMinutes();
-    var s = date.getSeconds();
+    let y = date.getFullYear();
+    let m = date.getMonth()+1;
+    let d = date.getDate();
+    let h = date.getHours();
+    let mm = date.getMinutes();
+    let s = date.getSeconds();
     return y+'-'+add0(m)+'-'+add0(d)+' '+add0(h)+':'+add0(mm)+':'+add0(s);
 }
 function add0(m){return m<10?'0'+m:m }
+
+
+Array.prototype.remove = function(s) {
+    for (let i = 0; i < this.length; i++) {
+        if (s === this[i])
+            this.splice(i, 1);
+    }
+}
+
+function Map() {
+    /** 存放键的数组(遍历用到) */
+    this.keys = [];
+    /** 存放数据 */
+    this.data = {};
+    /**
+     * 放入一个键值对
+     * @param {String} key
+     * @param {Object} value
+     */
+    this.put = function(key, value) {
+        if(this.data[key] == null){
+            this.keys.push(key);
+        }
+        this.data[key] = value;
+    };
+    /**
+     * 获取某键对应的值
+     * @param {String} key
+     * @return {Object} value
+     */
+    this.get = function(key) {
+        return this.data[key];
+    };
+    /**
+     * 删除一个键值对
+     * @param {String} key
+     */
+    this.remove = function(key) {
+        this.keys.remove(key);
+        this.data[key] = null;
+    };
+    /**
+     * 遍历Map,执行处理函数
+     *
+     * @param fn {Function} 回调函数 function(key,value,index){..}
+     */
+    this.each = function(fn){
+        if(typeof fn != 'function'){
+            return;
+        }
+        let len = this.keys.length;
+        for(let i = 0; i < len; i++){
+            let k = this.keys[i];
+            fn(k,this.data[k],i);
+        }
+    };
+
+    /**
+     * 获取键值数组(类似Java的entrySet())
+     * @return any[]{key,value}的数组
+     */
+    this.entrys = function() {
+        let len = this.keys.length;
+        let entrys = new Array(len);
+        for (let i = 0; i < len; i++) {
+            entrys[i] = {
+                key : this.keys[i],
+                value : this.data[i]
+            };
+        }
+        return entrys;
+    };
+    /**
+     * 判断Map是否为空
+     */
+    this.isEmpty = function() {
+        return this.keys.length === 0;
+    };
+    /**
+     * 获取键值对数量
+     */
+    this.size = function(){
+        return this.keys.length;
+    };
+    /**
+     * 重写toString
+     */
+    this.toString = function(){
+        let s = "{";
+        for(let i=0;i<this.keys.length;i++,s+=','){
+            let k = this.keys[i];
+            s += k+"="+this.data[k];
+        }
+        s+="}";
+        return s;
+    };
+}
--- src/main/resources/static/bootstrap/js/proto/proto.js
+++ src/main/resources/static/bootstrap/js/proto/proto.js
@@ -1,22 +1,54 @@
 /*Websocket沟通协议相关*/
 
-function ProtoConst() {
-    return {
-        baseType : "baseType"
-        ,
+/*基础协议类型*/
+const baseType = "baseType";
+/*请求类型*/
+const reqType = "reqType";
+/*基础协议内的子协议字符串*/
+const protoString = "protoString";
+/*响应类型*/
+const resType = "resType";
+/*响应体携带的真正json数据或字符串*/
+const message = "message";
+
+const protoMap = new Map();
+{
+    // 所有的基础协议都需要在这里添加，请参照类：BaseProtoType.java，否则无法注册
+    protoMap.put("BASE_HEART_BEAT", new Map());
+    protoMap.put("BASE_CHAT_HALL", new Map());
+
+    // 最后打印所有
+    console.log("基础协议：" + protoMap.toString());
+}
+function registerWSHandler(baseProtoName, subProtoName, callback) {
+    let currentProto = protoMap.get(baseProtoName);
+    if (currentProto == null) {
+        console.log("处理类注册失败！未知的基础协议名称：" + baseProtoName);
+    } else {
+        currentProto.put(subProtoName, callback);
     }
 }
 
-/*基础协议类型*/
-// const baseType = "baseType";
-// /*基础协议内的子协议字符串*/
-// const protoString = "protoString";
-// /*请求类型*/
-// const reqType = "reqType";
-// /*响应类型*/
-// const resType = "resType";
-// const baseProto = {
-//     BASE_HEART_BEAT : "BASE_HEART_BEAT"
-//     ,
-// };
-
+// 处理服务端返回的响应
+function handleResponse(ws, event) {
+    // 字符串转json
+    let baseProtoData = JSON.parse(event.data);
+    // 基础协议的类型，参考 WsProtoConstants
+    let baseProtoType = baseProtoData[baseType];
+    let subProtoMap = protoMap.get(baseProtoType);
+    if (subProtoMap == null) {
+        alert("未知的基础协议类型：" + event.data["baseType"]);
+    } else {
+        // 子协议内容
+        let subProtoData = JSON.parse(baseProtoData[protoString]);
+        // 子协议类型
+        let subProtoType = subProtoData[resType];
+        let callback = subProtoMap.get(subProtoType);
+        if (callback == null) {
+            alert("基础协议名：" + baseProtoType + "，未知的子协议名称：" + subProtoType);
+        } else {
+            // 回调
+            callback(subProtoData);
+        }
+    }
+}
